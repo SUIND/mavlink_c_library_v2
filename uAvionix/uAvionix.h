6579cdfce6bfefd6b1779a092280a9cfe745c508
Kunal Shrivastava <kunalshrivastava.iitr@gmail.com>
2025-11-21 20:06:08 +0530

    added suind mission data msg

--- uAvionix/uAvionix.h
+++ uAvionix/uAvionix.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_UAVIONIX.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_UAVIONIX_XML_HASH 652161426708278391
+#define MAVLINK_UAVIONIX_XML_HASH 428434109830106377
 
 #ifdef __cplusplus
 extern "C" {
